647f4803d3887ae39162a9a1334249fe48000ddc
Alan Mendelevich <ailon@ailon.org>
2025-04-02 14:31:47 +0300

    add editor layout structure

--- src/lib/AnnotationEditor.ts
+++ src/lib/AnnotationEditor.ts
@@ -2,6 +2,9 @@ import styles from "./lib.css?inline";
 
 export class AnnotationEditor extends HTMLElement {
   private _mainContainer?: HTMLDivElement;
+  private _toolbarContainer?: HTMLDivElement;
+  private _toolboxContainer?: HTMLDivElement;
+  private _markerAreaContainer?: HTMLDivElement;
 
   constructor() {
     super();
@@ -28,13 +31,34 @@ export class AnnotationEditor extends HTMLElement {
   private createLayout() {
     this._mainContainer = document.createElement("div");
     this._mainContainer.id = "mainContainer";
-    this._mainContainer.style.width = "100%";
-    this._mainContainer.style.height = "100%";
-    this._mainContainer.style.display = "flex";
+    this._mainContainer.className =
+      "grid grid-rows-[auto_1fr_auto] w-full h-full";
 
-    // tmp
-    this._mainContainer.innerHTML = `<p class="text-3xl hover:text-red-400">Hello World!</p>
-    <button class="btn btn-primary">Button</button>`;
+    this._toolbarContainer = document.createElement("div");
+    this._toolbarContainer.id = "toolbarContainer";
+
+    this._toolbarContainer.innerHTML = `<p>toolbar</p>`;
+
+    this._mainContainer.appendChild(this._toolbarContainer);
+
+    this._markerAreaContainer = document.createElement("div");
+    this._markerAreaContainer.id = "markerAreaContainer";
+    this._markerAreaContainer.className = "flex overflow-hidden bg-slate-50";
+
+    this._markerAreaContainer.innerHTML = `<p>marker area</p>`;
+
+    this._mainContainer.appendChild(this._markerAreaContainer);
+
+    this._toolboxContainer = document.createElement("div");
+    this._toolboxContainer.id = "toolboxContainer";
+
+    this._toolboxContainer.innerHTML = `<p>toolbox</p>`;
+
+    this._mainContainer.appendChild(this._toolboxContainer);
+
+    // // tmp
+    // this._mainContainer.innerHTML = `<p class="text-3xl hover:text-red-400">Hello World!</p>
+    // <button class="btn btn-primary">Button</button>`;
 
     this.shadowRoot?.appendChild(this._mainContainer);
   }
