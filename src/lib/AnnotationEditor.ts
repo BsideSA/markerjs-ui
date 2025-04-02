93c08797d35d0488ecb6f3749b17fe93a28bc29c
Alan Mendelevich <ailon@ailon.org>
2025-04-02 14:04:20 +0300

    add basic component structure

--- src/lib/AnnotationEditor.ts
+++ src/lib/AnnotationEditor.ts
@@ -1,13 +1,32 @@
 export class AnnotationEditor extends HTMLElement {
+  private _mainContainer?: HTMLDivElement;
+
   constructor() {
     super();
+
+    this.createLayout = this.createLayout.bind(this);
 
     this.attachShadow({ mode: "open" });
   }
 
-  connectedCallback() {}
+  connectedCallback() {
+    this.createLayout();
+  }
 
   disconnectedCallback() {}
+
+  private createLayout() {
+    this._mainContainer = document.createElement("div");
+    this._mainContainer.id = "mainContainer";
+    this._mainContainer.style.width = "100%";
+    this._mainContainer.style.height = "100%";
+    this._mainContainer.style.display = "flex";
+
+    // tmp
+    this._mainContainer.innerHTML = "<p>Hello World!</p>";
+
+    this.shadowRoot?.appendChild(this._mainContainer);
+  }
 }
 
 if (
