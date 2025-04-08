93204cff75d10b4e413eb9d7c33054413b63f711
Alan Mendelevich <ailon@ailon.org>
2025-04-08 10:24:37 +0300

    extract color picker into a separate class

---
+++ src/lib/ColorPicker.ts
@@ -0,0 +1,66 @@
+export class ColorPicker {
+  private _colors: string[] = [];
+  private _colorButtons: HTMLButtonElement[] = [];
+
+  private _currentColor: string = "#000000";
+  public get currentColor(): string {
+    return this._currentColor;
+  }
+  public set currentColor(value: string) {
+    this._currentColor = value;
+    this.updateButtonStates();
+  }
+
+  public onColorChange: (color: string) => void = () => {};
+
+  constructor(colors: string[]) {
+    this._colors = colors;
+
+    this.getUI = this.getUI.bind(this);
+    this.updateButtonStates = this.updateButtonStates.bind(this);
+  }
+
+  public getUI(): HTMLDivElement {
+    const colorButtonsContainer = document.createElement("div");
+    colorButtonsContainer.className = "flex flex-wrap";
+
+    this._colors.forEach((color) => {
+      const button = document.createElement("button");
+      button.className = "btn btn-square btn-sm btn-ghost base-content p-1.5";
+      button.setAttribute("aria-label", color);
+      button.setAttribute("data-color", color);
+
+      if (color !== "transparent") {
+        button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path fill="${color}" stroke-width="2" stroke="currentColor" d="M0,0 H24 V24 H0 Z" /></svg>`;
+      } else {
+        button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path fill="${color}" stroke-width={2} stroke="currentColor" d="M0,0 H24 V24 H0 Z" /><path fill="currentColor" opacity="0.2" stroke-width="0" d="M0,0 H12 V12 H0 Z" /><path fill="currentColor" opacity="0.2" stroke-width="0" d="M12,12 H24 V24 H12 Z" /></svg>`;
+      }
+
+      button.addEventListener("click", (event) => {
+        const target = event.currentTarget as HTMLButtonElement;
+        this._currentColor =
+          target.getAttribute("data-color") || this._colors[0];
+        // this.applyValues();
+        this.updateButtonStates();
+        if (this.onColorChange) {
+          this.onColorChange(this._currentColor);
+        }
+      });
+      this._colorButtons.push(button);
+      colorButtonsContainer.appendChild(button);
+    });
+
+    return colorButtonsContainer;
+  }
+
+  private updateButtonStates() {
+    this._colorButtons.forEach((button) => {
+      const color = button.getAttribute("data-color") || "";
+      if (color === this._currentColor) {
+        button.classList.add("btn-active");
+      } else {
+        button.classList.remove("btn-active");
+      }
+    });
+  }
+}
--- src/lib/FillToolboxPanel.ts
+++ src/lib/FillToolboxPanel.ts
@@ -9,17 +9,16 @@ import {
 } from "@markerjs/markerjs3";
 import { ToolboxPanel } from "./ToolboxPanel";
 import { defaultColorsWithTransparent } from "./models/colors";
+import { ColorPicker } from "./ColorPicker";
 
 export class FillToolboxPanel extends ToolboxPanel {
   private _currentColor: string = defaultColorsWithTransparent[0];
-  private _colorButtons: HTMLButtonElement[] = [];
   private _currentEditor?: MarkerBaseEditor<MarkerBase>;
+  private _colorPicker?: ColorPicker;
 
   constructor(markerArea: MarkerArea, title: string, icon: string) {
     super(markerArea, title, icon);
     this.width = "w-54";
-
-    this.updateButtonStates = this.updateButtonStates.bind(this);
   }
 
   protected override getContentUI() {
@@ -31,44 +30,28 @@ export class FillToolboxPanel extends ToolboxPanel {
     legend.innerText = "Fill Color";
     fieldset.appendChild(legend);
 
-    const colorButtonsContainer = document.createElement("div");
-    colorButtonsContainer.className = "flex flex-wrap";
+    this._colorPicker = new ColorPicker(defaultColorsWithTransparent);
+    this._colorPicker.onColorChange = (color: string) => {
+      this._currentColor = color;
+      this.applyValues();
+    };
 
-    defaultColorsWithTransparent.forEach((color) => {
-      const button = document.createElement("button");
-      button.className = "btn btn-square btn-sm btn-ghost base-content p-1.5";
-      button.setAttribute("aria-label", color);
-      button.setAttribute("data-color", color);
-
-      if (color !== "transparent") {
-        button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path fill="${color}" stroke-width="2" stroke="currentColor" d="M0,0 H24 V24 H0 Z" /></svg>`;
-      } else {
-        button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path fill="${color}" stroke-width={2} stroke="currentColor" d="M0,0 H24 V24 H0 Z" /><path fill="currentColor" opacity="0.2" stroke-width="0" d="M0,0 H12 V12 H0 Z" /><path fill="currentColor" opacity="0.2" stroke-width="0" d="M12,12 H24 V24 H12 Z" /></svg>`;
-      }
-
-      button.addEventListener("click", (event) => {
-        const target = event.currentTarget as HTMLButtonElement;
-        this._currentColor =
-          target.getAttribute("data-color") || defaultColorsWithTransparent[0];
-        this.applyValues();
-        this.updateButtonStates();
-      });
-      this._colorButtons.push(button);
-      colorButtonsContainer.appendChild(button);
-    });
-
-    fieldset.appendChild(colorButtonsContainer);
+    fieldset.appendChild(this._colorPicker.getUI());
 
     return fieldset;
   }
 
   public override updateContent(): void {
-    if (this._markerArea && this._markerArea.currentMarkerEditor) {
+    if (
+      this._markerArea &&
+      this._markerArea.currentMarkerEditor &&
+      this._colorPicker
+    ) {
       this._currentEditor = this._markerArea.currentMarkerEditor;
       this._currentColor =
         this._markerArea.currentMarkerEditor.fillColor ||
         defaultColorsWithTransparent[0];
-      this.updateButtonStates();
+      this._colorPicker.currentColor = this._currentColor;
     }
   }
 
@@ -76,17 +59,6 @@ export class FillToolboxPanel extends ToolboxPanel {
     if (this._markerArea && this._currentEditor) {
       this._currentEditor.fillColor = this._currentColor;
     }
-  }
-
-  private updateButtonStates() {
-    this._colorButtons.forEach((button) => {
-      const color = button.getAttribute("data-color") || "";
-      if (color === this._currentColor) {
-        button.classList.add("btn-active");
-      } else {
-        button.classList.remove("btn-active");
-      }
-    });
   }
 
   public override updateVisibility(): void {
