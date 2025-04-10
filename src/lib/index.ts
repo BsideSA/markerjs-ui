3924d04104d8862fdea720a1b51983838bc89eae
Alan Mendelevich <ailon@ailon.org>
2025-04-10 14:05:21 +0300

    add export for custom event data types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,13 @@
-import { AnnotationEditor } from "./AnnotationEditor";
+import {
+  AnnotationEditor,
+  AnnotationEditorEventData,
+  AnnotationEditorRenderEventData,
+} from "./AnnotationEditor";
 import { AnnotationViewer } from "./AnnotationViewer";
 
-export { AnnotationEditor, AnnotationViewer };
+export {
+  AnnotationEditor,
+  AnnotationViewer,
+  type AnnotationEditorEventData,
+  type AnnotationEditorRenderEventData,
+};
